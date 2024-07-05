8230573a0afaecdfb7cab2cd8a63af821f9f2cd6
Gopal Sharma <gopal200@gmail.com>
2024-07-05 05:02:02 +0000

    Under-SubFolder

---
+++ LTSPFx-Default/lib/webparts/extension/components/festivals.module.scss.js
@@ -0,0 +1,32 @@
+/* tslint:disable */
+require("./festivals.module.css");
+var styles = {
+    lightrope: 'lightrope_bdcc07b5',
+    'flash-1': 'flash-1_bdcc07b5',
+    'flash-2': 'flash-2_bdcc07b5',
+    'flash-3': 'flash-3_bdcc07b5',
+    text: 'text_bdcc07b5',
+    'text-animation': 'text-animation_bdcc07b5',
+    bombrocket: 'bombrocket_bdcc07b5',
+    normalrocket: 'normalrocket_bdcc07b5',
+    'bombrocket-animate-2': 'bombrocket-animate-2_bdcc07b5',
+    'normalrocket-animate-2': 'normalrocket-animate-2_bdcc07b5',
+    'bombrocket-animate-3': 'bombrocket-animate-3_bdcc07b5',
+    'normalrocket-animate-3': 'normalrocket-animate-3_bdcc07b5',
+    'bombrocket-animate-1': 'bombrocket-animate-1_bdcc07b5',
+    'normalrocket-animate-1': 'normalrocket-animate-1_bdcc07b5',
+    clouds: 'clouds_bdcc07b5',
+    santa: 'santa_bdcc07b5',
+    'santa-run': 'santa-run_bdcc07b5',
+    ground: 'ground_bdcc07b5',
+    tree: 'tree_bdcc07b5',
+    'move-tree': 'move-tree_bdcc07b5',
+    treelarge: 'treelarge_bdcc07b5',
+    treebackground: 'treebackground_bdcc07b5',
+    'move-background': 'move-background_bdcc07b5',
+    caption: 'caption_bdcc07b5',
+    'caption--small': 'caption--small_bdcc07b5'
+};
+export default styles;
+/* tslint:enable */ 
+//# sourceMappingURL=festivals.module.scss.js.map
