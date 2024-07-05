0158cf85e93be693afc399e5eb1e62b2586b8c1d
Gopal Sharma <gopal200@gmail.com>
2024-07-05 07:35:43 +0000

    new Updates

--- LTSPFx-Default/src/webparts/ltspFx/LtspFxWebPart.ts
+++ LTSPFx-Default/src/webparts/ltspFx/LtspFxWebPart.ts
@@ -30,19 +30,9 @@ export default class LtspFxWebPart extends BaseClientSideWebPart<ILtspFxWebPartP
       </div>
       <div>
         <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
+        <h4><p>
+        LeapThought SharePoint Framework (SPFx).....
+        </p></h4>
       </div>
     </section>`;
   }
